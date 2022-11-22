cfbe0f07cb1d615bd39c1c6533cd1fa4bb327d23
Jennifer Parak <jenpaff0@gmail.com>
2022-11-22 21:58:28 +0100

    add contract using recursion API

---
+++ contracts/src/RecSchnitzel.ts
@@ -0,0 +1,271 @@
+import {
+  Field,
+  SmartContract,
+  state,
+  State,
+  method,
+  isReady,
+  CircuitValue,
+  prop,
+  Bool,
+  Experimental,
+  Poseidon,
+  Circuit,
+  UInt32,
+  Proof,
+  SelfProof,
+} from 'snarkyjs';
+import geohash from 'ngeohash';
+import { MerkleTree } from 'snarkyjs/dist/node/lib/merkle_tree.js';
+/**
+ * Basic Example
+ * See https://docs.minaprotocol.com/zkapps for more info.
+ *
+ * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
+ * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
+ *
+ * This file is safe to delete and replace with your own contract.
+ */
+
+await isReady;
+
+export { MerkleWitness, Solution1Tree, Solution2Tree, Solution3Tree };
+export {
+  RecSchnitzelApp,
+  RecSchnitzelHuntState,
+  RecSchnitzelHelper,
+  RecSchnitzelRollup,
+};
+
+const height = 11;
+const Solution1Tree = new Experimental.MerkleTree(height);
+const Solution2Tree = new Experimental.MerkleTree(height);
+const Solution3Tree = new Experimental.MerkleTree(height);
+class MerkleWitness extends Experimental.MerkleWitness(height) {}
+
+export class LocationCheck extends CircuitValue {
+  @prop sharedGeoHash: Field;
+
+  constructor(lat: number, long: number) {
+    super();
+    var geoHash: number = geohash.encode_int(lat, long);
+    this.sharedGeoHash = Field(geoHash);
+    console.log(
+      'geoHash hash: ' + Poseidon.hash(this.sharedGeoHash.toFields())
+    );
+  }
+
+  hash(): Field {
+    return Poseidon.hash(this.sharedGeoHash.toFields());
+  }
+}
+
+class RecSchnitzelHuntState extends CircuitValue {
+  @prop step: UInt32;
+  @prop solution1Root: Field;
+  @prop solution2Root: Field;
+  @prop solution3Root: Field;
+
+  constructor(
+    step: UInt32,
+    solution1Root: Field,
+    solution2Root: Field,
+    solution3Root: Field
+  ) {
+    super();
+    this.step = step;
+    this.solution1Root = solution1Root;
+    this.solution2Root = solution2Root;
+    this.solution3Root = solution3Root;
+  }
+
+  // helper
+  static from(state: {
+    step: UInt32;
+    solution1Root: Field;
+    solution2Root: Field;
+    solution3Root: Field;
+  }) {
+    return new this(
+      state.step,
+      state.solution1Root,
+      state.solution2Root,
+      state.solution3Root
+    );
+  }
+}
+
+let RecSchnitzelApp = Experimental.ZkProgram({
+  publicInput: RecSchnitzelHuntState,
+
+  methods: {
+    init: {
+      privateInputs: [],
+
+      method(publicInput: RecSchnitzelHuntState) {
+        // check that values are correctly initialised
+        publicInput.step.assertEquals(UInt32.zero);
+        const solution1Root = publicInput.solution1Root;
+        publicInput.solution1Root.assertEquals(solution1Root);
+
+        const solution2Root = publicInput.solution2Root;
+        publicInput.solution2Root.assertEquals(solution2Root);
+
+        const solution3Root = publicInput.solution3Root;
+        publicInput.solution3Root.assertEquals(solution3Root);
+      },
+    },
+    hunt: {
+      privateInputs: [LocationCheck, MerkleWitness, SelfProof],
+
+      method(
+        publicInput: RecSchnitzelHuntState,
+        sharedLocation: LocationCheck,
+        path: MerkleWitness,
+        previousProof: SelfProof<RecSchnitzelHuntState>
+      ) {
+        previousProof.verify();
+
+        let step = previousProof.publicInput.step;
+        step = step.add(UInt32.one);
+        publicInput.step.assertEquals(step);
+
+        const solution1Root = publicInput.solution1Root;
+        publicInput.solution1Root.assertEquals(solution1Root);
+
+        const solution2Root = publicInput.solution2Root;
+        publicInput.solution2Root.assertEquals(solution2Root);
+
+        const solution3Root = publicInput.solution3Root;
+        publicInput.solution3Root.assertEquals(solution3Root);
+
+        const isFirstStep: Bool = step.equals(UInt32.one);
+        const isSecondStep: Bool = step.equals(UInt32.from(2));
+        const isThirdStep: Bool = step.equals(UInt32.from(3));
+
+        const root_to_check = Circuit.switch(
+          [isFirstStep, isSecondStep, isThirdStep],
+          Field,
+          [solution1Root, solution2Root, solution3Root]
+        );
+
+        path
+          .calculateRoot(Poseidon.hash(sharedLocation.sharedGeoHash.toFields()))
+          .assertEquals(root_to_check);
+      },
+    },
+  },
+});
+
+class RecSchnitzelProof extends Proof<RecSchnitzelHuntState> {
+  static publicInputType = RecSchnitzelHuntState;
+  static tag = () => RecSchnitzelApp;
+}
+
+class RecSchnitzelRollup extends SmartContract {
+  @state(Bool) finished = State<Bool>();
+
+  @method finish(
+    proof: RecSchnitzelProof // <-- we're passing in a proof!
+  ) {
+    // verify the proof
+    proof.verify();
+
+    // assert that user completed all steps
+    proof.publicInput.step.assertEquals(UInt32.from(3));
+
+    // declare that someone won this game!
+    this.finished.set(Bool(true));
+  }
+}
+
+let RecSchnitzelHelper = {
+  init(solution1Root: Field, solution2Root: Field, solution3Root: Field) {
+    return RecSchnitzelHuntState.from({
+      step: UInt32.zero,
+      solution1Root,
+      solution2Root,
+      solution3Root,
+    });
+  },
+
+  generateMerkleProof(
+    sharedLocation: LocationCheck,
+    solution1Map: Map<string, number>,
+    solution2Map: Map<string, number>,
+    solution3Map: Map<string, number>,
+    previousProof: SelfProof<RecSchnitzelHuntState>
+  ): MerkleWitness {
+    // TOFIX: got a weird error when i couldn't match UInt32 in my switch case
+    // cos this function would then returned an undefined witness
+    // returns an OCaml error
+    let step = previousProof.publicInput.step.toString();
+    let idx;
+    let witness;
+
+    switch (step) {
+      case '0':
+        console.log('attempt to solve step 0');
+        idx = solution1Map.get(sharedLocation.sharedGeoHash.toString());
+        if (idx == undefined) {
+          throw console.log('Location shared is incorrect!');
+        }
+        witness = new MerkleWitness(Solution1Tree.getWitness(BigInt(+idx)));
+        break;
+      case '1':
+        console.log('attempt to solve step 1');
+        idx = solution2Map.get(sharedLocation.sharedGeoHash.toString());
+        if (idx == undefined) {
+          throw console.log('Location shared is incorrect!');
+        }
+        witness = new MerkleWitness(Solution2Tree.getWitness(BigInt(+idx)));
+        break;
+      case '2':
+        console.log('attempt to solve step 2');
+        idx = solution3Map.get(sharedLocation.sharedGeoHash.toString());
+        if (idx == undefined) {
+          throw console.log('Location shared is incorrect!');
+        }
+        witness = new MerkleWitness(Solution3Tree.getWitness(BigInt(+idx)));
+        break;
+      default:
+        throw console.log('Invalid step: ' + step);
+    }
+    if (step == undefined || witness == undefined) {
+      throw console.log(
+        'Step invalid: ' + step + ' - Witness invalid: ' + witness
+      );
+    }
+
+    return witness;
+  },
+
+  hunt(previousProof: SelfProof<RecSchnitzelHuntState>) {
+    return RecSchnitzelHuntState.from({
+      ...previousProof.publicInput,
+      step: previousProof.publicInput.step.add(UInt32.one),
+    });
+  },
+};
+
+export function generate_solution_tree(
+  minlat: number,
+  minlong: number,
+  maxlat: number,
+  maxlong: number,
+  tree: MerkleTree
+): Map<string, number> {
+  let solutionMap = new Map<string, number>();
+  const solution = geohash
+    .bboxes_int(minlat, minlong, maxlat, maxlong)
+    .toString()
+    .split(',');
+  for (let index = 0; index < solution.length; index++) {
+    let map_index = BigInt(index);
+    let hash = Poseidon.hash(Field(+solution[index]).toFields());
+    console.log('index: ' + index + ' geohash HASH: ' + hash);
+    solutionMap.set(solution[index], index);
+    tree.setLeaf(map_index, hash);
+  }
+  return solutionMap;
+}
---
+++ contracts/src/run-recursive.ts
@@ -0,0 +1,260 @@
+import {
+  LocationCheck,
+  Solution1Tree,
+  Solution2Tree,
+  Solution3Tree,
+  generate_solution_tree,
+} from './Schnitzel.js';
+import {
+  AccountUpdate,
+  Field,
+  Mina,
+  Poseidon,
+  PrivateKey,
+  shutdown,
+} from 'snarkyjs';
+import geohash from 'ngeohash';
+import { tic, toc } from './tictoc.js';
+import {
+  RecSchnitzelApp,
+  RecSchnitzelHelper,
+  RecSchnitzelRollup,
+} from './RecSchnitzel.js';
+/*
+    when turned off it only adds one geohash solution to the solutionTree (used for quick testing/showcasing) 
+    rather than loading the whole solution tree to allow for a wider range of allowed locations per solution
+  */
+const doQuick = true;
+
+let Solution1Map = new Map<string, number>(); // mapping geohash to index
+let Solution2Map = new Map<string, number>(); // mapping geohash to index
+let Solution3Map = new Map<string, number>(); // mapping geohash to index
+
+if (doQuick) {
+  // solution 1
+  const geohash_solution1 = geohash.encode_int(48.2107958217, 16.3736155926);
+  let hash = Poseidon.hash(Field(+geohash_solution1).toFields());
+  Solution1Map.set(geohash_solution1.toString(), 0);
+  Solution1Tree.setLeaf(BigInt(0), hash);
+
+  // solution 2
+  const geohash_solution2 = geohash.encode_int(48.2079410492, 16.3716678382);
+  let hash2 = Poseidon.hash(Field(+geohash_solution2).toFields());
+  Solution2Map.set(geohash_solution2.toString(), 0);
+  Solution2Tree.setLeaf(BigInt(0), hash2);
+
+  // solution 3
+  const geohash_solution3 = geohash.encode_int(48.2086269882, 16.3725081062);
+  let hash3 = Poseidon.hash(Field(+geohash_solution3).toFields());
+  Solution3Map.set(geohash_solution3.toString(), 0);
+  Solution3Tree.setLeaf(BigInt(0), hash3);
+} else {
+  tic('build solution merkle trees');
+  // write_solution_map_to_file(
+  //   'solution1.txt',
+  //   48.2107356534,
+  //   16.3736139593,
+  //   48.2108048225,
+  //   16.3737322524
+  // );
+  Solution1Map = generate_solution_tree(
+    48.2107356534,
+    16.3736139593,
+    48.2108048225,
+    16.3737322524,
+    Solution1Tree
+  );
+  // write_solution_map_to_file(
+  //   'solution2.txt',
+  //   48.2079049216,
+  //   16.3716384673,
+  //   48.2079451583,
+  //   16.3717048444
+  // );
+  Solution2Map = generate_solution_tree(
+    48.2079049216,
+    16.3716384673,
+    48.2079451583,
+    16.3717048444,
+    Solution2Tree
+  );
+  // write_solution_map_to_file(
+  //   'solution3.txt',
+  //   48.2085512071,
+  //   16.3723948785,
+  //   48.208693012,
+  //   16.372583382
+  // );
+  Solution3Map = generate_solution_tree(
+    48.2086269882,
+    16.3725081062,
+    48.2086858438,
+    16.3725546748,
+    Solution3Tree
+  );
+  toc();
+}
+
+console.log('root 1' + Solution1Tree.getRoot());
+console.log('root 2' + Solution2Tree.getRoot());
+console.log('root 2' + Solution3Tree.getRoot());
+
+console.log('solution1Map size ' + Solution1Map.size);
+console.log('solution2Map size ' + Solution2Map.size);
+console.log('Solution3Map size ' + Solution3Map.size);
+
+tic('compiling');
+await RecSchnitzelApp.compile();
+toc();
+
+tic('prove (init)');
+let initialState = RecSchnitzelHelper.init(
+  Solution1Tree.getRoot(),
+  Solution2Tree.getRoot(),
+  Solution3Tree.getRoot()
+);
+let initialProof = await RecSchnitzelApp.init(initialState);
+toc();
+
+console.log('Proof state initialized!');
+
+console.log(
+  `\n ==================================================== \n Solving 1st out of 2 riddles, should update state accordingly...`
+);
+
+// TODO: add error case
+
+tic('prove (riddle 1)');
+let location1 = new LocationCheck(48.2107958217, 16.37361559266);
+let location1Witness = RecSchnitzelHelper.generateMerkleProof(
+  location1,
+  Solution1Map,
+  Solution2Map,
+  Solution3Map,
+  initialProof
+);
+let location1State = RecSchnitzelHelper.hunt(initialProof);
+let location1Proof = await RecSchnitzelApp.hunt(
+  location1State,
+  location1,
+  location1Witness,
+  initialProof
+);
+toc();
+
+console.log('Solved riddle 1');
+console.log('json proof: ' + location1Proof.toJSON().proof);
+
+let step = location1State.step.toString();
+
+// assert state
+console.log('State step: ', step);
+
+if (step != '1') {
+  throw Error('Did not increase step after successfully solving 1st riddle');
+}
+
+// TODO: add error case here too before submitting correct 2nd step
+
+console.log(
+  `\n ==================================================== \n Solving 2nd out of 3 riddles with correct location...`
+);
+
+tic('prove (riddle 2)');
+let location2 = new LocationCheck(48.2079410492, 16.3716678382);
+let location2Witness = RecSchnitzelHelper.generateMerkleProof(
+  location2,
+  Solution1Map,
+  Solution2Map,
+  Solution3Map,
+  location1Proof
+);
+let location2State = RecSchnitzelHelper.hunt(location1Proof);
+let location2Proof = await RecSchnitzelApp.hunt(
+  location2State,
+  location2,
+  location2Witness,
+  location1Proof
+);
+toc();
+
+console.log('Solved riddle 2');
+console.log('json proof: ' + location2Proof.toJSON().proof);
+
+// assert state is correctly updated after correct move
+
+console.log('State step: ', step);
+
+step = location2State.step.toString();
+
+if (step != '2') {
+  throw Error('Did not increase step after successfully solving 2nd riddle');
+}
+
+console.log(
+  `\n ==================================================== \n Solving 3rd out of 3 riddles, should now update state to true with correct location...`
+);
+
+tic('prove (riddle 3)');
+let location3 = new LocationCheck(48.2086269882, 16.3725081062);
+let location3Witness = RecSchnitzelHelper.generateMerkleProof(
+  location3,
+  Solution1Map,
+  Solution2Map,
+  Solution3Map,
+  location2Proof
+);
+let location3State = RecSchnitzelHelper.hunt(location2Proof);
+let location3Proof = await RecSchnitzelApp.hunt(
+  location3State,
+  location3,
+  location3Witness,
+  location2Proof
+);
+toc();
+
+console.log('Solved riddle 3 - final proof!');
+console.log('json proof: ' + location3Proof.toJSON().proof);
+
+// TODO: compile & deploy rollup
+
+let zkAppPrivateKey = PrivateKey.random();
+let zkAppAddress = zkAppPrivateKey.toPublicKey();
+let zkapp = new RecSchnitzelRollup(zkAppAddress);
+
+let Local = Mina.LocalBlockchain();
+Mina.setActiveInstance(Local);
+const publisherAccount = Local.testAccounts[0].privateKey;
+
+// deploy
+tic('compile & deploy rollup');
+await RecSchnitzelRollup.compile();
+let tx = await Mina.transaction(publisherAccount, () => {
+  AccountUpdate.fundNewAccount(publisherAccount);
+  zkapp.deploy({ zkappKey: zkAppPrivateKey });
+});
+await tx.send().wait();
+toc();
+
+// prove that we have a proof that shows that we won
+tic('prove (rollup)');
+tx = await Mina.transaction(publisherAccount, () => {
+  // call out method with final proof from the ZkProgram as argument
+  zkapp.finish(location3Proof);
+});
+await tx.prove();
+await tx.send().wait();
+toc();
+
+let solved = zkapp.finished.get().toBoolean();
+
+// assert state is correctly updated after correct move
+console.log('State finished: ', solved);
+
+if (solved != true) {
+  throw Error('Did not complete game after solving all riddles');
+}
+
+console.log('Finished recursive schnitzelhunt ! :) ');
+
+shutdown();
